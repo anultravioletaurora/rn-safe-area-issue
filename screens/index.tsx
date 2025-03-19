44a23be3fa58b60960d37927aac80b0e2b6ab55e
Violet Caulfield <violet@cosmonautical.cloud>
2025-03-19 17:59:41 -0500

    adding packages, update app.tsx

---
+++ screens/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index(): React.JSX.Element {
+    return (
+        <View>
+            <Text>Index</Text>
+        </View>
+    )
+}
